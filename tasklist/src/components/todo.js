eb8fe009dc320810299d05f7f4bdf0f390ff2631
Rubens Rangel <rubensrangjob802@gmail.com>
2023-05-04 09:20:18 -0400

    todo component creacted

---
+++ tasklist/src/components/todo.js
@@ -0,0 +1,3 @@
+export default function Todo({item}){
+    return <div>{item.title}</div>
+}
--- tasklist/src/components/todoApp.js
+++ tasklist/src/components/todoApp.js
@@ -1,3 +1,44 @@
+
+import { useState } from "react";
+import Todo from "./todo";
+
+
+
 export default function TodoApp(){
-    return <div>hola</div>
+    const [title, setTitle] = useState("HOLA");
+    const [todo, setTodo] = useState([]);
+
+    
+
+    function handleChange(event){
+        const value = event.target.value;
+        setTitle(value)
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        const newTodo = {
+            id: crypto.randomUUID(),
+            title: title,
+            completed: false
+        }
+
+        setTodo([...todo, newTodo])
+    }
+
+    return <div className="todoConteiner">
+        <form className="todoCreateForm" onSubmit={handleSubmit}>
+            <input onChange={handleChange} className="todoInput" value={title}/>
+            <input onClick={handleSubmit} type="submit" value="Create todo" className="buttonCreate"/>
+        </form>
+
+        <div className="todosConteiner">
+            {
+            todo.map((item) =>  (
+                <Todo key={item.id} item={item}/>
+                ))
+            }
+        </div>
+        
+    </div>;
 }
